d8c72953d3b927699dcca52c5be5092d7a85d66d
taghouti <taghouti@users.noreply.github.com>
2026-06-21 21:03:43 +0100

    chore: bump version to 2.0.6

--- conky_manager.py
+++ conky_manager.py
@@ -40,7 +40,7 @@ AUTOSTART_DIR = HOME / ".config" / "autostart"
 DATA_DIR = HOME / ".local" / "share" / "conky-manager"
 SETTINGS_FILE = DATA_DIR / "settings.json"
 LOG_FILE = DATA_DIR / "manager.log"
-VERSION = "2.0.5"
+VERSION = "2.0.6"
 REPO_URL = "https://github.com/taghouti-org/conky-manager.git"
 
 # Supported archive extensions
